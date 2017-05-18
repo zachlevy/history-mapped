71977cd7e4a3b0fd69560ff3d8683c70b64c04e2
Zachary Levy <zacharyalevy@gmail.com>
2017-05-18 19:42:13 -0400

    youtube player

--- src/Moment.js
+++ src/Moment.js
@@ -1,10 +1,22 @@
 import React, { Component } from 'react'
 
+import YouTube from 'react-youtube'
+
 class Moment extends Component {
   render() {
     const moment = this.props.moment
+    const playerOptions = {
+      videoId: moment.youtubeId,
+      height: '390',
+      width: '100%'
+    }
     return (
       <div>
+        <YouTube
+          videoId={moment.youtubeId}
+          opts={playerOptions}
+          onReady={this._onReady}
+        />
         <h4>{moment.title}</h4>
         <p>{moment.youtubeId}</p>
         <p>{new Date(moment.date).getUTCFullYear()}</p>
