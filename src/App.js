be1a9bb488b1f651036f738a1eb6f39328e226f2
Zachary Levy <zacharyalevy@gmail.com>
2017-05-18 16:35:49 -0400

    basic css for map

--- src/App.js
+++ src/App.js
@@ -10,35 +10,34 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <ReactMapboxGl
-          style="mapbox://styles/mapbox/streets-v8"
-          accessToken="pk.eyJ1IjoiemFjaGxldnkiLCJhIjoiY2lobWExbHJyMG8yNnQ0bHpmYW1zZXV2YyJ9.5RDwdgrQtOdHCOapEwe6eA"
-          containerStyle={{
-            height: "100vh",
-            width: "100vw"
-          }}>
+        <div>
+          <ReactMapboxGl
+            style="mapbox://styles/mapbox/streets-v8"
+            accessToken="pk.eyJ1IjoiemFjaGxldnkiLCJhIjoiY2lobWExbHJyMG8yNnQ0bHpmYW1zZXV2YyJ9.5RDwdgrQtOdHCOapEwe6eA"
+            containerStyle={{height: "100vh", width: "100vw"}}
+            >
             <Layer
               type="symbol"
               id="marker"
               layout={{ "icon-image": "marker-15" }}>
-              <Feature coordinates={[-0.481747846041145, 51.3233379650232]}/>
+              {
+                videos.map((video, index) => {
+                  return <Feature key={index} coordinates={[video.location.lng, video.location.lat]}/>
+                })
+              }
             </Layer>
-        </ReactMapboxGl>
+          </ReactMapboxGl>
+        </div>
         <table>
-        {
-          videos.sort((a, b) => { return new Date(a.date) - new Date(b.date)}).map((video, index) => {
-            return (
-              <Video key={index} video={video}></Video>
-            )
-          })
-        }
+          <tbody>
+          {
+            videos.sort((a, b) => { return new Date(a.date) - new Date(b.date)}).map((video, index) => {
+              return (
+                <Video key={index} video={video}></Video>
+              )
+            })
+          }
+          </tbody>
         </table>
       </div>
     );
