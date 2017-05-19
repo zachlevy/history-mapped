54f036c7275637310a276dae72c8c4f270217fa2
Zachary Levy <zacharyalevy@gmail.com>
2017-05-18 20:48:48 -0400

    refactored moment and selected moment video

--- src/Moment.js
+++ src/Moment.js
@@ -11,18 +11,49 @@ class Moment extends Component {
       height: '390',
       width: '100%'
     }
-    return (
-      <div>
+    const selectedMoment = this.props.timelineIndex == 1
+    if (selectedMoment) {
+      return (
         <div>
-          {this.props.timelineIndex == 1 && !this.props.mapMoving ? <YouTube videoId={moment.youtubeId} opts={playerOptions} onReady={this._onReady} /> : null }
+          <div className="row">
+            <div className="col-12">
+              <h4>{moment.title}</h4>
+              <p>{moment.author}</p>
+              <p>{dateToCommonEra(new Date(moment.date))}</p>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-12">
+              {!this.props.mapMoving ? <YouTube videoId={moment.youtubeId} opts={playerOptions} onReady={this._onReady} /> : null }
+            </div>
+          </div>
+          <hr />
         </div>
-        <h4>{moment.title}</h4>
-        <p>{dateToCommonEra(new Date(moment.date))}</p>
-        <p>{moment.author}</p>
-        <button onClick={this.props.handleClick.bind(null, moment)}>Location</button>
-        <hr />
-      </div>
-    )
+      )
+    } else {
+      return (
+        <div>
+          <div className="row">
+            <div className="col-12">
+              <h4>{moment.title}</h4>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-12 col-sm-8">
+              <p>{moment.author}</p>
+              <p>{dateToCommonEra(new Date(moment.date))}</p>
+            </div>
+            <div className="col-12 col-sm-4">
+              <button className="btn btn-primary" onClick={this.props.handleClick.bind(null, moment)}>Watch Battle</button>
+            </div>
+          </div>
+          <hr />
+        </div>
+      )
+    }
+
+
+
   }
 }
 
