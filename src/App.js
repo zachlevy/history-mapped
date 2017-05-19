4d3ee82d709d5e1b84d58b7573c0a6396017527d
Zachary Levy <zacharyalevy@gmail.com>
2017-05-19 16:53:36 -0400

    spear icon instead

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ class App extends Component {
   onMapLoad(map, event) {
     console.log("onMapLoad")
     this.map = map
-    map.loadImage('/icons/shield_icon_black_red_x256.png', (error, image) => {
+    map.loadImage('/icons/spear_icon_back_red_x256.png', (error, image) => {
       if (error) throw error
       map.addImage('battle-icon', image)
     })
